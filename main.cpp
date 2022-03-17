0328d2cbcb1b29254942d4ad2a321f1b13d50e77
Luis Alvarez <mlpa16.2@gmail.com>
2022-03-17 21:13:57 +0800

    added basic structure

--- main.cpp
+++ main.cpp
@@ -1,2 +1,36 @@
 //code
 //alvarez angeles tuyay 126 proj 1
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(){
+  bool run=1;
+  string cmd;
+
+  while(run!=0){
+    cin<<cmd;
+    
+    if(cmd==){
+
+    }
+    else if(cmd==){
+      
+    }
+    else if(cmd==){
+      
+    }
+    else if(cmd==){
+      
+    }
+    else if(cmd==){
+      
+    }
+    else{
+
+    }
+
+  }
+
+}
