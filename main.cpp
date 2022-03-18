43056715922f7bb450d554682eee669abe294c2b
Luis Alvarez <mlpa16.2@gmail.com>
2022-03-18 16:36:22 +0800

    added

--- main.cpp
+++ main.cpp
@@ -5,13 +5,29 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <algorithm>
 using namespace std;
+
+void fillparams(vector<string> &cmdlist, vector<char> &operators,vector<char> &arguments){
+  cmdlist.push_back("commands"); //populate this with our commands
+  cmdlist.push_back("end");
+
+
+  operators.push_back('<'); //populate this with the operators
+  operators.push_back('>');
+  operators.push_back('|');
+}
+
 
 
 int main(){
   bool run=1;
   string in,tempstr;
   vector<string> cmdvec;
+  vector<string> cmdlist;
+  vector<string> arguments;
+  vector<char> operators;
+  fillparams(cmdlist,operators,arguments);
 
   while(run!=0){
     getline(cin,in);
@@ -20,6 +36,16 @@ int main(){
     while(cmd>>tempstr){
       cmdvec.push_back(tempstr);
     }
+
+    if(find(cmdlist.begin(),cmdlist.end(),cmdvec[0])!=cmdlist.end()){
+      string command=cmdvec[0];
+      cout<<cmdvec[0]<<" valid";
+    }
+    else{
+      cout<<cmdvec[0]<<" invalid";
+    }
+
+    
 
     
   }
