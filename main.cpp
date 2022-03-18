457459074e097308c29c9157bc3b13999a62b4f3
Luis Alvarez <mlpa16.2@gmail.com>
2022-03-18 13:31:24 +0800

    added getline input and vector for arguments

--- main.cpp
+++ main.cpp
@@ -1,36 +1,27 @@
 //code
 //alvarez angeles tuyay 126 proj 1
 #include <iostream>
+#include <Bits.h>
 #include <string>
+#include <sstream>
+#include <vector>
 using namespace std;
 
 
 int main(){
   bool run=1;
-  string cmd;
+  string in,tempstr;
+  vector<string> cmdvec;
 
   while(run!=0){
-    cin<<cmd;
+    getline(cin,in);
+    stringstream cmd(in);
+    cmdvec.clear();
+    while(cmd>>tempstr){
+      cmdvec.push_back(tempstr);
+    }
+
     
-    if(cmd==){
-
-    }
-    else if(cmd==){
-      
-    }
-    else if(cmd==){
-      
-    }
-    else if(cmd==){
-      
-    }
-    else if(cmd==){
-      
-    }
-    else{
-
-    }
-
   }
 
 }
