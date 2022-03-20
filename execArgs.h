bf8e68a2217fdf3d3de4a9eb409f8cab423ec228
Luis Alvarez <mlpa16.2@gmail.com>
2022-03-21 01:15:42 +0800

    invaded poland and made blank header

--- execArgs.h
+++ execArgs.h
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,9 +7,69 @@
 #include <sys/types.h> 
 #include <sys/wait.h> 
 #include <sys/stat.h>
+*/
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
 using namespace std;
 
+void invadepoland(vector<string> &args, int len)
+{
+    pid_t child1;
+    int flag=0;
+    if(args[len-1]=="&")
+    {
+        flag=1;
+        args[len-1]=NULL;
+        len--;
+    }
+    child1=fork();
+
+    if(child1==0)
+    {
+        if(len>=3)
+        {
+            if(args[len-2]==">")
+            {
+                args[len-2]=NULL;
+                //whatever those arguments for linux open are
+                int out = open(args[len - 1], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+                // switch from stdout to file
+                dup2(out, STDOUT_FILENO);
+                close(out);
+            }
+            else if(args[len-2]=="<")
+            {
+                args[len-2]=NULL;
+                // open input file
+                int in = open(args[len - 1], O_RDONLY);
+                //redirect input from converter to get from file
+                dup2(in, STDIN_FILENO);
+                close(in);
+            }
+        }
+        if (execvp(args[0], args) < 0){
+            cout << "\nwell that didnt work";
+        }
+        exit(1);
+    }
+    else if(child1 > 0){ // if parent is running
+    // check if theres no &
+    if (flag==0)
+    {
+        wait(NULL);
+    }
+        return;
+    }else {
+
+        cout << "\nL+ratio+no maidens+split failed"; //cant split process
+        return;
+    }
+}
+
+
+/*
 #define MAX_LINE 80
 //non-pipe regular function
 void execArgs(char *args[], int len){
@@ -64,3 +125,4 @@ void execArgs(char *args[], int len){
         return;
     }
 }
+*/
