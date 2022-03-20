f4f518b7be08ac6a95ea4bb456e8d60d66c62a04
KapitanKraus <mkraus.angeles@gmail.com>
2022-03-20 17:50:30 +0800

    main is now main program

---
+++ execArgsPipe.h
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h> 
+#include <sys/wait.h> 
+#include <sys/stat.h>
+#include <iostream>
+using namespace std;
+
+#define MAX_LINE 80
+// execute function uses pipe()
+void execArgsPipe(char *args[], char *argsPipe[], int flag){
+    __pid_t pid;
+
+    int fd[2];// file decription
+    if(pipe(fd) < 0){//initialize pipeline
+        cout << "\nCan't init pipe!!!!";
+        return;
+    }
+    // process split
+    pid = fork();
+
+    if(pid == 0){// Tiến trình con
+        dup2(fd[1], STDOUT_FILENO);// write to file 
+        // closed unused section
+        close(fd[0]);
+        // Close the used fd[2] part 
+        close(fd[1]);
+        //check if executable
+        if(execvp(args[0],args) < 0){
+            cout << "\nCan't execute pipe 1...";
+        }
+        exit(1);
+    }else if(pid >0){//parent program
+        
+        pid = fork();
+        if(pid == 0){
+            dup2(fd[0], STDIN_FILENO);
+            close(fd[1]);
+            close(fd[0]);
+            if(execvp(argsPipe[0], argsPipe) < 0){
+                cout << "\nCan't execute pipe 2...";
+            }
+            
+            exit(1);
+        }
+        else if(pid > 0){
+            int state;
+            close(fd[0]);
+            close(fd[1]);
+            if(flag==0)
+                waitpid(pid, &state, 0);
+            return;
+        }
+        else {
+            cout << "\nCan't fork!!!";
+            return;
+        }
+    }
+    else{
+        cout << "\nCan't fork!!!";
+        return;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,86 +1,117 @@
 //code
 //alvarez angeles tuyay 126 proj 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h> 
+#include <sys/wait.h> 
+#include <sys/stat.h>
 #include <iostream>
-#include <Bits.h>
-#include <string>
-#include <sstream>
-#include <vector>
-#include <algorithm>
+#include <execArgsPipe.h>
+#include <execArgs.h>
+
 using namespace std;
 
-void fillparams(vector<string> &cmdlist, vector<string> &operators){
-  cmdlist.push_back("commands"); //populate this with our commands
-  cmdlist.push_back("end");
+#define MAX_LINE 80
 
-
-  operators.push_back("<"); //populate this with the operators
-  operators.push_back(">");
-  operators.push_back("|");
+//input function
+int getInput(char input[], char his[]){
+    cout << "execute";
+    fflush(stdout);
+    
+     fgets(input,MAX_LINE,stdin);
+    
+    input[strlen(input)-1]='\0';
+    if(strcmp(input, "!!") == 0){
+        if(strlen(his) != 0){
+            cout << "%s\n" << his;
+            strcpy(input, his);
+        }
+        else{
+            cout << "No command in history!!!\n";
+            return -1;
+        }
+    }
+    else if(strcmp(input,"exit")==0)
+        return 0;
+    if(strlen(input) > 0){
+        strcpy(his, input);
+    }
+    return 1;
 }
 
 
 
-int main(){
-  bool run=1,validcmd=0;
-  string in,tempstr;
-  vector<string> cmdvec;
-  vector<string> cmdlist;
-  vector<string> cmds;
-  vector<string> arguments; //list
-  vector<string> operators; //list
-
-  vector<string> ops;
-  vector<string> args;
-
-  fillparams(cmdlist,operators);
-
-  while(run!=0){
-    getline(cin,in);
-    stringstream cmd(in);
-    cmdvec.clear();
-    while(cmd>>tempstr){
-      cmdvec.push_back(tempstr);
+//parse function
+int parseInput(char *args[], char input[]){
+    int count = 0;
+    char *tok = strtok(input," ");
+    while(tok != NULL){
+        args[count++] = tok;
+        tok = strtok(NULL, " ");
     }
+    args[count] = tok;
+    return count;
+}
 
-    if(find(cmdlist.begin(),cmdlist.end(),cmdvec[0])!=cmdlist.end()){
-      //cout<<cmdvec[0]<<" valid";
-      validcmd=1;
-      for(int i=0;i<cmdvec.size();i++){
-        if(find(cmdlist.begin(),cmdlist.end(),cmdvec[i])!=cmdlist.end()){
-          cmds.push_back(cmds[i]);
+//The function checks whether the passed argument uses a pipe or not
+int checkPipe(char *args[], int len){
+    for(int i = 0; i < len; i++){
+        if(strcmp(args[i], "|") == 0)
+            return i;
+    }
+    return -1;
+}
+
+
+
+//general execute function
+void genExec(char *args[], int len){
+    int flag=0;
+    int iPipe = checkPipe(args, len);
+    if(iPipe == -1){
+        execArgs(args, len);
+        return;
+    } else {
+        char *argsPipe[MAX_LINE / 2 + 1];
+        int i;
+        for(i = 0; i < len - iPipe - 1; i++){
+            argsPipe[i] = strdup(args[iPipe + i +1]);
         }
-        else{}
-        if(find(operators.begin(),operators.end(),cmdvec[i])!=operators.end()){
-          ops.push_back(cmdvec[i]);
-          args.push_back(cmdvec[i+1]);
+
+        argsPipe[i] = NULL; 
+        if(*argsPipe[i-1]=='&'){
+            flag=1;
+            argsPipe[i-1] = NULL;
         }
-      }
+        args[iPipe] = NULL;
+        execArgsPipe(args, argsPipe,flag);
+        return;
+    }
+}
 
-        /*debug parse checker
-        cout<<cmdvec[0]<<endl;
-        for(int i=0;i<ops.size();i++){
-          cout<<ops[i]<<" ";
+int main(void)
+{
+    char *args[MAX_LINE / 2 + 1];
+    char his[100];
+    his[0] = '\0';
+    int runProg = 1;
+    int state = 0;
+
+    while(runProg) {
+        char input[100];
+        int len;
+        int res=getInput(input, his);
+        if( res== 1){//execute command
+            len = parseInput(args, input);
+            genExec(args, len);
         }
-        cout<<endl;
-        for(int i=0;i<args.size();i++){
-          cout<<args[i]<<" ";
+        else if(res==0)// exit
+        {
+            runProg=0;
         }
-        */
-      }
-
-      else{
-        cout<<cmdvec[0]<<"\ninvalid command,";
-      }
-
-    
-
-
-
-
-
-
-
-    
-  }
-
+    }
+    return 0;
 }
