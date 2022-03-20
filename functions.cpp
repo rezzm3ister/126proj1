a6f34f578f8a74c139653c5eca7358b26ca5c9c6
Luis Alvarez <mlpa16.2@gmail.com>
2022-03-21 02:21:14 +0800

    more libs

--- functions.cpp
+++ functions.cpp
@@ -3,6 +3,9 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <iostream>
 #include <string>
 #include <sstream>
--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <sys/types.h> 
 #include <sys/wait.h> 
 #include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
 #include <iostream>
